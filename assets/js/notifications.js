1f591cab09759b0a38b39533da1607a8dc68e092
keymeat2025 <keymeat2025@gmail.com>
2025-04-25 17:24:23 +0530

    Create notifications.js
    created newly for photographer-dashboard.html

---
+++ assets/js/notifications.js
@@ -0,0 +1,7 @@
+/**
+ * Notifications system for the dashboard
+ */
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Notifications system initialized');
+    // Notification functionality will be implemented here in the future
+});
